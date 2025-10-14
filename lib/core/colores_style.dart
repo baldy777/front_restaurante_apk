94630fcb43b5f8ef844c167c7dfae21fd140278f
baldy777 <jhoelquispetapia315@gmail.com>
2025-10-14 19:49:40 -0400

    feat: creacion de paleta principal de estilos de colores

---
+++ lib/core/colores_style.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class ColoresStyle {
+  static const Color fondo = Color(0xFFF6F1EB); 
+  static const Color encabezado = Color(0xFF7B8C38);
+  static const Color acento = Color(0xFF5A3E2B);
+  static const Color detalles = Color(0xFFA8B59E);
+  static const Color texto = Color(0xFF2E2E2E);
+  
+}
