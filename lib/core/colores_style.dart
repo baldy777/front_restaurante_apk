f596835395271aad0570a763b01dd6e514138181
baldy777 <jhoelquispetapia315@gmail.com>
2025-10-14 22:34:05 -0400

    perf: reescribir los estilos de texto de la app

--- lib/core/colores_style.dart
+++ lib/core/colores_style.dart
@@ -1,10 +1,17 @@
 import 'dart:ui';
 
 class ColoresStyle {
-  static const Color fondo = Color(0xFFF6F1EB); 
+
+  //colores de la app
+  static const Color fondo = Color(0xFFF6F1EB);
   static const Color encabezado = Color(0xFF7B8C38);
   static const Color acento = Color(0xFF5A3E2B);
   static const Color detalles = Color(0xFFA8B59E);
-  static const Color texto = Color(0xFF2E2E2E);
-  
+
+  //estilos de texto
+  static final TextStyle texto = TextStyle(
+    fontFamily: "arial",
+    fontSize: 18,
+    color: const Color.fromARGB(255, 236, 230, 225),
+  );
 }
