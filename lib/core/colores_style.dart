52cc0397796fbcd9059cdea06b74ea10af0dfecc
baldy777 <jhoelquispetapia315@gmail.com>
2025-11-23 20:26:34 -0400

    style: actualizado de paleta de colores

--- lib/core/colores_style.dart
+++ lib/core/colores_style.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 
 class ColoresStyle {
   //colores de la app
-  static const Color fondo = Color(0xFF5D6D7E);
-  static const Color encabezado = Color(0xFF1B2631);
-  static const Color acento = Color(0xFF34495E);
+  static const Color fondo = Color(0xFFD6DBDF);
+  static const Color encabezado = Color(0xFF9E4E2F);
+  static const Color acento = Color(0xFF5E392B);
   static const Color detalles = Color(0xFFD6DBDF);
 }
 
@@ -13,7 +13,7 @@ class TextoStyle {
     color: Color(0xFFF6F1EB),
     fontSize: 24,
     fontFamily: 'arial',
-    fontWeight: FontWeight.bold
+    fontWeight: FontWeight.bold,
   );
   static const TextStyle contenido = TextStyle(
     color: Color(0xFF2E2E2E),
